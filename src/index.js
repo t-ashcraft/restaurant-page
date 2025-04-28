395f71a15924fce2dc27a0d587521938ff6891eb
t-ashcraft <termisdash@gmail.com>
2025-04-28 15:21:13 -0400

    put together header and footer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
+import "./style.css";
+
+
 const body = document.querySelector("body");
 
-const header = document.createElement("div");
-const footer = document.createElement("div");
-const main = document.createElement("div");
+const header = document.querySelector("header")
+const footer = document.querySelector("footer")
+const content = document.querySelector("#content");
 
-function createPage() {
-    header.id = "header";
-    footer.id = "footer";
-    main.id = "main";
-
-
-    body.appendChild(header);
-    body.appendChild(main);
-    body.appendChild(footer);
-}
 
